a02f5de7f6651601c899f49c4c53a395eff079b1
Sarah Maris <sarah.87.maris@gmail.com>
2015-12-28 11:19:04 -0500

    Add framework for each test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -40,7 +40,7 @@ $(function() {
          * and that the name is not empty.
          */
 
-         it('has a non-empty URL', function(){
+         it('has a non-empty name', function(){
 
          });
 
@@ -49,35 +49,51 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The Menu', function() {
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden by default', function() {
+
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('changes visibility when the menu icon is clicked', function() {
+
+          });
+
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
+
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it('includes at least one entry element', function() {
+
+         });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection"*/
     describe('New Feed Selection', function() {
+
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         it('is loaded and content changes', function() {
+
+         });
     });
 
 }());
