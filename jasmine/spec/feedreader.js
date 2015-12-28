02ee27fd90962a56b2ba8167f55e90bf8167434c
Sarah Maris <sarah.87.maris@gmail.com>
2015-12-28 12:16:43 -0500

    Define tests for 'RSS Feeds'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,41 +9,39 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
+    // Confirm that RSS feeds are defined in our application.
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
+        //Confirm that the allFeeds variable is defined and not empty
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
+         // Function to loop through feeds and confirm that feed url and name
+         // are defined and not empty
+         function feedsDefined(feed) {
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('has a non-empty URL', function(){
+            describe(feed.name, function() {
 
-         });
+                //Check that url is defined and not empty
+                it('has a non-empty URL', function(){
+                    expect(feed.url).toBeTruthy();
+                });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+                //Check that name is defined and not empty
+                it('has a non-empty name', function(){
+                    expect(feed.name).toBeTruthy();
+                });
 
-         it('has a non-empty name', function(){
+            });
+        }
 
-         });
-
+        //Test to confirm that each feed is defined
+        allFeeds.forEach(function (feed) {
+            feedsDefined(feed);
+        });
     });
 
 
